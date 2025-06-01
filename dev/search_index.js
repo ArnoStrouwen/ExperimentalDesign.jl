da4b9b0fe2030f93d25beb775847c72e4352d837
Documenter.jl <documenter@juliadocs.github.io>
2025-06-01 02:12:41 +0000

    build based on b7513d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ExperimentalDesign","page":"Home","title":"ExperimentalDesign","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"using ExperimentalDesign\ndesign_space = [DiscreteNumericFactor(:T, 3), DiscreteNumericFactor(:P, [1, 3]), CategoricalFactor(:Level, [\"A\", \"B\"])]\ndesign = solve(FullFactorial(), design_space)","category":"page"}]
+}
